ac986d112fd1e06e9690ff312ffd7e52b4568ef9
Vish <savishy.pv@gmail.com>
2018-10-08 13:49:57 +0530

    python calc tdd - committing WIP code

--- string-calculator/python/calculator.py
+++ string-calculator/python/calculator.py
@@ -1,6 +1,6 @@
 import re
 def Add(input):
-    inp_array = re.split('[\n,]+',input)
+    inp_array = parse_delim(input)
     result = 0
     for inp in inp_array:
         if get_int(inp) <= 1000:
@@ -11,5 +11,15 @@ def get_int(str):
     s = str.strip()
     return int(s) if s else 0
 
+# Parse Delim and Split String
+def parse_delim(str):
+    delims_raw = re.search(r'(?<=//)(.*)(?=\n)', str)
+    if delims_raw:
+        print delims_raw.group(0)
+        delim = delims_raw.group(0)
+        return re.split(delim,str)
+    else:
+        return re.split('[\n,]+',str)
+    
 class NegativeNumberException(Exception):
     pass
